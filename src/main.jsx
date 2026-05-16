6c11f84aa0f791eef34e5342699d8a217e54d6d2
lalisforoni <lalisforoni@gmail.com>
2026-05-16 07:56:59 -0300

    Atualizar o main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Pumpi from '../Pumpi.jsx'
+import Pumpi from './Pumpi.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
