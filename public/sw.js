9876f1b0e77b151b122dba49361b0a201e2dd3ae
lalisforoni <lalisforoni@gmail.com>
2026-05-29 13:06:33 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(clients.claim()));
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
 });
