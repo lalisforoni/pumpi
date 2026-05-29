4e89ed470cdffea94408e2b28f407882e56f7c37
lalisforoni <lalisforoni@gmail.com>
2026-05-29 13:18:05 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom/client'
 import Pumpi from './Pumpi.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Pumpi />
-  </React.StrictMode>
+  <Pumpi />
 )
